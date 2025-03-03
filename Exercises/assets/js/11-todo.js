251c14823efe5f80ffc9db2b7681ffd78f1afc12
hananmuzaffar <hananmuzaffar26@yahoo.com>
2025-03-03 14:24:18 +0530

    Some exercises of Arrays and loops

--- Exercises/assets/js/11-todo.js
+++ Exercises/assets/js/11-todo.js
@@ -1,5 +1,19 @@
 const todoList = [];
 
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHTML = '';
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoListHTML += html;
+  }
+
+  document.querySelector('.task-list')
+    .innerHTML = todoListHTML;
+}
+
 function addTodo() {
   const inputElement = document.querySelector('.task-name');
   const name = inputElement.value;
@@ -7,4 +21,6 @@ function addTodo() {
   todoList.push(name);
   console.log(todoList);
   inputElement.value = '';
+
+  renderTodoList();
 }
