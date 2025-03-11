cc4a20df3d1b344373c5f1aa0c6f841e568d9349
hananmuzaffar <hananmuzaffar26@yahoo.com>
2025-03-11 12:43:42 +0530

    Advanced Functions & Ecercises

--- Exercises/assets/js/12-todo.js
+++ Exercises/assets/js/12-todo.js
@@ -4,23 +4,19 @@ renderTodoList();
 
 function renderTodoList() {
   let todoListHTML = '';
-  for (let i = 0; i < todoList.length; i++) {
-    const todoObject = todoList[i];
-    //const name = todoObject.name;
-    //const dueDate = todoObject.dueDate;
+  todoList.forEach(function(todoObject, index) {
     const {name, dueDate} = todoObject;
     const html = `
                   <div>${name}</div>
                   <div>${dueDate}</div>
                   <button class="delete-btn" onclick="
-                    todoList.splice(${i}, 1);
+                    todoList.splice(${index}, 1);
                     renderTodoList();
                     storeTask();
                   ">Delete</button>
                 `;
     todoListHTML += html;
-  }
-
+  });
   document.querySelector('.task-list')
     .innerHTML = todoListHTML;
 }
