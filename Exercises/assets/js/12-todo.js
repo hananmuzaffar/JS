756e5b989a2aa30b5a289ae023a650fe546c5ae9
hananmuzaffar <hananmuzaffar26@yahoo.com>
2025-03-13 16:52:48 +0530

    Arrow functions

--- Exercises/assets/js/12-todo.js
+++ Exercises/assets/js/12-todo.js
@@ -4,22 +4,35 @@ renderTodoList();
 
 function renderTodoList() {
   let todoListHTML = '';
-  todoList.forEach(function(todoObject, index) {
+  todoList.forEach((todoObject, index) => {
     const {name, dueDate} = todoObject;
     const html = `
                   <div>${name}</div>
                   <div>${dueDate}</div>
-                  <button class="delete-btn" onclick="
-                    todoList.splice(${index}, 1);
-                    renderTodoList();
-                    storeTask();
-                  ">Delete</button>
+                  <button class="delete-btn">Delete</button>
                 `;
     todoListHTML += html;
   });
   document.querySelector('.task-list')
     .innerHTML = todoListHTML;
+
+  document.querySelectorAll('.delete-btn')
+    .forEach((deleteBtn, index) => {
+      deleteBtn.addEventListener('click', () =>{
+        todoList.splice(index, 1);
+        renderTodoList();
+        storeTask();
+      })
+    });
+    
 }
+
+document.querySelector('.add-btn')
+  .addEventListener('click', () => {
+    addTodo();
+  });
+
+  
 
 function addTodo() {
   const taskInputElement = document.querySelector('.task-name');
